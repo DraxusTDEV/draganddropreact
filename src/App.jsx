f1bf5d801d7a923d8a1df512dfdb1900b1a99f00
DraxusTDEV <draxustienda@gmail.com>
2024-02-11 20:43:15 -0500

    añadiendo lista

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const initialTodos = [
+  {
+    id: 1,
+    text: "Aprender React.js",
+  },
+  {
+    id: 2,
+    text: "Aprender JS",
+  },
+  {
+    id: 3,
+    text: "Aprender Vue.js",
+  },
+];
+
+const App = () => {
+  const [todos, setTodos] = useState(initialTodos);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Drag and Drop</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
